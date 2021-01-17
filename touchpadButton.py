51c641f5876ef17477065162a635b84429b9415e
alexknop <knopalex@msu.edu>
2021-01-17 13:26:55 -0500

    Update touchpadButton.py
    hid details

--- touchpadButton.py
+++ touchpadButton.py
@@ -4,13 +4,15 @@ from evdev import InputDevice, categorize, ecodes, list_devices
 #creates object 'gamepad' to store the data
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 for device in devices:
-    if device.name == "Wireless Controller Touchpad":
+    #change name to name of your device
+    if device.name == "<DEVICE NAME>":
         gamepad = InputDevice(device.path)
 
 
 for event in gamepad.read_loop():
-    #break if you find a touchpad press
+    #break if you find a button press
     if event.type == ecodes.EV_KEY:
-        if event.code == 272:
+        #change event code to the code of your button
+        if event.code == <button code>:
             print("1")
             break
